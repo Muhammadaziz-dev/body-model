590b9c6b323eef2b78b8aebadaba0496584e8f57
Muhammadazizprogromer <103125035+Muhammadazizprogromer@users.noreply.github.com>
2023-03-18 19:17:39 +0500

    body-model

---
+++ readme2.txt
@@ -0,0 +1,2 @@
+npm install @tensorflow-models/body-pix @tensorflow/tfjs react-webcam mashini install qilish kere
+keyin npm start qilish kere bo'ladi
--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect } from "react"
+import * as tf from "@tensorflow/tfjs"
+import * as bodyPix from "@tensorflow-models/body-pix"
+import Webcam from 'react-webcam'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const webcamRef = useRef(null)
+  const canvasRef = useRef(null)
+
+  const runBodysegment = async() => {
+    const net = await bodyPix.load()
+    setInterval(() => {
+      detect(net)
+    }, 100)
+  }
+
+  const detect = async(net) => {
+    if (
+      typeof webcamRef.current !== 'undefined' &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video
+      const videoWidth = webcamRef.current.video.videoWidth
+      const videoHeight = webcamRef.current.video.videoHeight
+
+      webcamRef.current.video.width = videoWidth
+      webcamRef.current.video.height = videoHeight
+
+      canvasRef.current.width = videoWidth
+      canvasRef.current.height = videoHeight
+
+      const person = await net.segmentPersonParts(video)
+
+      console.log(person)
+
+      const coloredPartIMage = bodyPix.toColoredPartMask(person)
+      const opacity = .7
+      const flipHorizontal = false
+      const maskBlurAmount = 0
+      const canvas = canvasRef.current
+
+      bodyPix.drawMask(canvas, video, coloredPartIMage, opacity, maskBlurAmount, flipHorizontal)
+    }
+  }
+
+  useEffect(() => {
+    runBodysegment()
+    
+  }, [])
+
+  return <div className="App">
+    <header className="App-header">
+      <Webcam className="Webcam" ref={webcamRef} />
+      <canvas className="canvas" ref={canvasRef} />
+    </header>
+  </div>
+    
+  
 }
 
-export default App;
+export default App
